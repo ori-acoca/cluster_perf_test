51a6a8a18acf09c5c44d66005aead0caa1aeec83
ori-acoca <ori.acoca@weka.io>
2024-09-05 17:53:20 +0300

    - Added tabulated results and banners. - Need to add support for testing less than 3 nodes in a cluster.

--- network_stress_test.py
+++ network_stress_test.py
@@ -1,16 +1,59 @@
 #!/usr/bin/python3
 import os
 import sys
+from tabulate import tabulate
 import inquirer
 import paramiko
 import time
+from colorama import Fore, Back, Style
 
 
+# Banners
+def banner_welcome():
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}                   Network Stress Test")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+def banner_dependency_check():
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}       Checking dependencies across cluster nodes")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+def banner_cleanup():
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}       Cleaning UP leftovers from previous tests")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+def banner_ipref():
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}       Testing iPerf all-to-all")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+def banner_results():
+    print()
+    print()
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}       Results")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+# Get creds from user
 class Credentials:
     def __init__(self):
         # Initialize the object with user input for two values
         self._username = input("Enter the username you are SSH with: \n")
-        self._key_name = input("\nEnter your SSH key name: \n")
+        print()
+        self._key_name = input("Enter your SSH key name: \n")
+        print()
 
     @property
     def username(self):
@@ -23,6 +66,7 @@ class Credentials:
         return self._key_name
 
 
+# Function for executing commands on remote nodes
 def execute_on_remote_nodes(host, command, creds, suppress_output=True):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -49,7 +93,7 @@ def execute_on_remote_nodes(host, command, creds, suppress_output=True):
 # Function to clean up test leftovers from nodes
 def cleanup_leftovers(nodes: list, creds):
     cleanup_command = "pkill -f 'iperf'; pkill -f 'ib_write_bw'; pkill -f 'ib_write_lat'"
-    print("\nCleaning leftovers from previous tests")
+    banner_cleanup()
     for node in nodes:
         execute_on_remote_nodes(node, cleanup_command, creds, suppress_output=True)
 
@@ -71,7 +115,7 @@ def read_node_list(link_layer):
         sys.exit()
 
 
-# Function to set up the server for testing
+# Setup server-side to accept connections
 def setup_server(link_layer, server, creds):
     if link_layer == "Ethernet":
         open_server = f"iperf -s '{server}'"
@@ -84,6 +128,7 @@ def setup_server(link_layer, server, creds):
     execute_on_remote_nodes(server, open_server, creds, suppress_output=True)
 
 
+# Setup client side to connect to server and collect results
 def run_client_test(link_layer, client, server, creds):
     if link_layer == "Ethernet":
         open_client = (
@@ -101,7 +146,7 @@ def run_client_test(link_layer, client, server, creds):
     return measured_perf
 
 
-# Function to perform all-to-all performance tests
+# Perform all-to-all performance tests
 def all_to_all(link_layer, node_list: list, creds):
     results = {}
 
@@ -115,6 +160,10 @@ def all_to_all(link_layer, node_list: list, creds):
                 results[server][client] = result
                 time.sleep(0.1)
     return results
+
+
+def tabulate_results(results):
+    print(tabulate(results, headers=["Server IP", "Throughput (Gbits/sec)"]))
 
 
 # Calculate the average of averages per server
@@ -138,11 +187,12 @@ def calc_avg(results):
 def sort_results(overall_avg_per_server):
     # Sort the servers based on overall average performance in descending order
     sorted_avg_per_server = dict(sorted(overall_avg_per_server.items(), key=lambda item: item[1], reverse=True))
+    return sorted_avg_per_server
 
-    # Print the sorted overall average per server
-    print("\nOverall Average per Server (Sorted):")
-    for server, avg in sorted_avg_per_server.items():
-        print(f"{server}:  {avg:.2f} Gbits/sec")
+
+def format_data_for_tabulate(sorted_avg_per_server):
+    formatted_results = [[server, throughput] for server, throughput in sorted_avg_per_server.items()]
+    return formatted_results
 
 
 def get_link_layer():
@@ -150,7 +200,7 @@ def get_link_layer():
     questions = [
         inquirer.List(
             'linklayer',
-            message="Layer2 protocol - Ethernet or InfiniBand ?",
+            message="Layer2 protocol ?",
             choices=['Ethernet', 'InfiniBand'],
         ),
     ]
@@ -159,15 +209,52 @@ def get_link_layer():
     return link_layer
 
 
+def check_dependencies(nodes: list, creds):
+    dependency_pass_nodes = []
+    dependency_fail_nodes = []
+
+    check_iperf = "if command -v iperf &> /dev/null; then echo 'Pass'; else echo 'Fail'; fi"
+    check_perftest = "if command -v ib_write_bw &> /dev/null; then echo 'Pass'; else echo 'Fail'; fi"
+
+    for node in nodes:
+        is_iperf_there = execute_on_remote_nodes(node, check_iperf, creds, suppress_output=False)
+        is_perftest_there = execute_on_remote_nodes(node, check_perftest, creds, suppress_output=False)
+
+        if "Pass" in is_iperf_there and is_perftest_there:
+            dependency_pass_nodes.append(node)
+        else:
+            dependency_fail_nodes.append(node)
+
+    return dependency_pass_nodes, dependency_fail_nodes
+
+
+def report_skipped_nodes(dependency_fail_nodes):
+    for node in dependency_fail_nodes:
+        print("Skipping " + node + " - Missing dependencies")
+
+
 def main():
+    banner_welcome()
     link_layer = get_link_layer()
     creds = Credentials()
     nodes = read_node_list(link_layer)
-    cleanup_leftovers(nodes=nodes, creds=creds)
-    results = all_to_all(link_layer=link_layer, node_list=nodes, creds=creds)
+    dependency_pass_nodes, dependency_fail_nodes = check_dependencies(nodes, creds)
+    banner_dependency_check()
+    report_skipped_nodes(dependency_fail_nodes)
+    cleanup_leftovers(nodes=dependency_pass_nodes, creds=creds)
+    banner_ipref()
+    results = all_to_all(link_layer=link_layer, node_list=dependency_pass_nodes, creds=creds)
     overall_avg_per_server = calc_avg(results)
-    sort_results(overall_avg_per_server)
-    cleanup_leftovers(nodes=nodes, creds=creds)
+    banner_results()
+    sorted_results = sort_results(overall_avg_per_server)
+    formatted_results = format_data_for_tabulate(sorted_results)
+    tabulate_results(formatted_results)
+    print("")
+    print("")
 
+    cleanup_leftovers(nodes=dependency_pass_nodes, creds=creds)
+    print("")
+    print("             <<<< Done >>>>                      ")
+    print()
 if __name__ == '__main__':
     main()
