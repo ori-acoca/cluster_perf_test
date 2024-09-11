c1429ab048448e71839bee8cb9bf7a61d7bbd6ee
ori-acoca <ori.acoca@weka.io>
2024-09-11 14:20:50 +0300

    Need to continue working on IB. The version before this is working with iPerf. This version should work as well.

--- network_stress_test.py
+++ network_stress_test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import os
 import sys
+from xmlrpc.client import boolean
 from tabulate import tabulate
 import inquirer
 import paramiko
 import time
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 
 # Banners
@@ -77,10 +78,10 @@ def execute_on_remote_nodes(host, command, creds, suppress_output=True):
             key_filename=os.path.join(os.path.expanduser('~'), ".ssh", creds.key_name)
         )
         _stdin, _stdout, _stderr = ssh.exec_command(command)
-
         if not suppress_output:
             output = _stdout.read().decode()
             ssh.close()
+            #print(output)
             return output
     except paramiko.SSHException as e:
         print(f"SSH error: {e}")
@@ -115,53 +116,80 @@ def read_node_list(link_layer):
         sys.exit()
 
 
-# Setup server-side to accept connections
-def setup_server(link_layer, server, creds):
-    if link_layer == "Ethernet":
-        open_server = f"iperf -s '{server}'"
-    elif link_layer == "InfiniBand":
-        open_server = f"ib_write_bw -s '{server}' --report_gbits"
-    else:
-        raise Exception('Unsupported link layer')
+def construct_server_command(link_layer, metric_to_test, server):
+    open_server = None
 
+    if link_layer == "Ethernet" and metric_to_test == "Bandwidth":
+        open_server = f"iperf -s '{server}'"
+    elif link_layer == "Ethernet" and metric_to_test == "Latency":
+        open_server = f"iperf -s '{server}'"
+
+    elif link_layer == "InfiniBand" and metric_to_test == "Bandwidth":
+        open_server = f"ib_write_bw -s '{server}' --report_gbits"
+    elif link_layer == "InfiniBand" and metric_to_test == "Latency":
+        open_server = f"ib_write_lat -s '{server}'"
+
+    return open_server
+
+
+# Setup server-side
+def run_server(link_layer, metric_to_test, server, creds):
+    open_server = construct_server_command(link_layer, metric_to_test, server)
     print('\n' + "###### " + server + " ######")
     execute_on_remote_nodes(server, open_server, creds, suppress_output=True)
 
 
-# Setup client side to connect to server and collect results
-def run_client_test(link_layer, client, server, creds):
-    if link_layer == "Ethernet":
-        open_client = (
-            f"iperf -c {server} -P 32 -f g | grep SUM | awk '{{print $6}}'"
-        )
-    elif link_layer == "InfiniBand":
-        open_client = f"ib_write_bw '{server}' --report_gbits"
-    else:
-        raise Exception('Unsupported link layer')
+def construct_client_command(link_layer, metric_to_test, server):
+    open_client = None
 
+    if link_layer == "Ethernet" and metric_to_test == "Bandwidth":
+        open_client = f"iperf -c {server} -P 32 -f g | grep SUM | awk '{{print $6}}'"
+    elif link_layer == "Ethernet" and metric_to_test == "Latency":
+        open_client = f"iperf -c {server} -P 32 -f g | grep SUM | awk '{{print $6}}'"
+
+    elif link_layer == "InfiniBand" and metric_to_test == "Bandwidth":
+        open_client = f"ib_write_bw '{server}' --report_gbits"
+    elif link_layer == "InfiniBand" and metric_to_test == "Latency":
+        open_client = f"ib_write_bw '{server}' -D 10"
+
+    return open_client
+
+
+# Setup client-side
+def run_client(link_layer, metric_to_test, client, server, creds):
+    open_client = construct_client_command(link_layer, metric_to_test, server)
     print(server + " <===> " + client)
-    measured_perf = execute_on_remote_nodes(client, open_client, creds, suppress_output=False)
-    # Remove any trailing whitespace or newline characters from the output
-    measured_perf = measured_perf.strip()
+    measured_perf = (execute_on_remote_nodes(client, open_client, creds, suppress_output=False))
+    measured_perf = float(measured_perf.strip())
     return measured_perf
 
 
 # Perform all-to-all performance tests
-def all_to_all(link_layer, node_list: list, creds):
+def all_to_all(link_layer, metric_to_test, node_list: list, creds):
+    banner_start_testing(link_layer, metric_to_test)
     results = {}
 
     for server in node_list:
-        setup_server(link_layer, server, creds)
+        run_server(link_layer, metric_to_test, server, creds)
         results[server] = {}
 
         for client in node_list:
             if client != server:
-                result = run_client_test(link_layer, client, server, creds)
+                result = run_client(link_layer, metric_to_test, client, server, creds)
                 results[server][client] = result
                 time.sleep(0.1)
+
     return results
 
 
+def banner_start_testing(link_layer, metric_to_test):
+    print(f"{Fore.GREEN}============================================================")
+    print(f"{Fore.GREEN}       Testing {metric_to_test} all-to-all for {link_layer}")
+    print(f"{Fore.GREEN}============================================================{Style.RESET_ALL}")
+    print()
+
+
+# Tabulate results in a table
 def tabulate_results(results):
     print(tabulate(results, headers=["Server IP", "Throughput (Gbits/sec)"]))
 
@@ -175,8 +203,7 @@ def calc_avg(results):
         client_count = 0
 
         for client in results[server]:
-            # Convert the string result to float and strip any extra whitespace or newline characters
-            avg_sum += float(results[server][client].strip())
+            avg_sum += (results[server][client])
             client_count += 1
 
         overall_avg_per_server[server] = avg_sum / client_count if client_count > 0 else 0.0
@@ -209,41 +236,105 @@ def get_link_layer():
     return link_layer
 
 
-def check_dependencies(nodes: list, creds):
-    dependency_pass_nodes = []
-    dependency_fail_nodes = []
+def get_metric_to_test():
+    # Gather network info from user
+    questions = [
+        inquirer.List(
+            'metric',
+            message="What metric do you want to test ?",
+            choices=['Bandwidth', 'Latency'],
+        ),
+    ]
+    answer = inquirer.prompt(questions)
+    metric = answer['metric']
+    return metric
 
-    check_iperf = "if command -v iperf &> /dev/null; then echo 'Pass'; else echo 'Fail'; fi"
-    check_perftest = "if command -v ib_write_bw &> /dev/null; then echo 'Pass'; else echo 'Fail'; fi"
 
+def init_nodes_attributes_dict(nodes: list):
+    nodes_attributes_dict = {}
     for node in nodes:
-        is_iperf_there = execute_on_remote_nodes(node, check_iperf, creds, suppress_output=False)
-        is_perftest_there = execute_on_remote_nodes(node, check_perftest, creds, suppress_output=False)
-
-        if "Pass" in is_iperf_there and is_perftest_there:
-            dependency_pass_nodes.append(node)
-        else:
-            dependency_fail_nodes.append(node)
-
-    return dependency_pass_nodes, dependency_fail_nodes
+        nodes_attributes_dict[node] = {
+            'ib_write_bw': "",
+            'ib_write_lat': "",
+            'iperf': "",
+        }
+    return nodes_attributes_dict
 
 
-def report_skipped_nodes(dependency_fail_nodes):
-    for node in dependency_fail_nodes:
-        print("Skipping " + node + " - Missing dependencies")
+def check_dependencies(nodes_attributes_dict: dict, creds):
+    for node, attributes in nodes_attributes_dict.items():
+        nodes_attributes_dict[node]["ib_write_bw"] = boolean(does_ib_write_bw_installed(node, creds))
+        nodes_attributes_dict[node]["ib_write_lat"] = boolean(does_ib_write_lat_installed(node, creds))
+        nodes_attributes_dict[node]["iperf"] = boolean(does_iperf_installed(node, creds))
+    return nodes_attributes_dict
+
+
+def does_ib_write_bw_installed(node, creds):
+    check_command = "which ib_write_bw"
+    output = execute_on_remote_nodes(node, check_command, creds, suppress_output=False)
+    if "ib_write_bw" in output:
+        return True
+    else:
+        return False
+
+
+def does_ib_write_lat_installed(node, creds):
+    check_command = "which ib_write_lat"
+    output = execute_on_remote_nodes(node, check_command, creds, suppress_output=False)
+    if "ib_write_lat" in output.lower():
+        return True
+    else:
+        return False
+
+
+def does_iperf_installed(node, creds):
+    check_command = "which iperf"
+    output = execute_on_remote_nodes(node, check_command, creds, suppress_output=False)
+    if "iperf" in output.lower():
+        return True
+    else:
+        return False
+
+
+def node_qualification(node_dependencies_status, link_layer, metric_to_test):
+    qualified_nodes = []
+    for node, status in node_dependencies_status.items():
+
+        if link_layer == "Ethernet":
+            if metric_to_test == "Bandwidth":
+                if status['iperf'] is False:
+                    print("Skipping " + node + " - is missing iperf or is broken")
+                else:
+                    qualified_nodes.append(node)
+
+        if link_layer == "InfiniBand":
+            if metric_to_test == "Bandwidth":
+                if not ['ib_write_bw']:
+                    print("Skipping " + node + " - is missing ib_write_bw or is broken")
+                else:
+                    qualified_nodes.append(node)
+
+            if metric_to_test == "Latency":
+                if not status['ib_write_lat']:
+                    print("Skipping " + node + " - is missing ib_write_lat or is broken")
+                else:
+                    qualified_nodes.append(node)
+
+    return qualified_nodes
 
 
 def main():
     banner_welcome()
     link_layer = get_link_layer()
+    metric_to_test = get_metric_to_test()
     creds = Credentials()
     nodes = read_node_list(link_layer)
-    dependency_pass_nodes, dependency_fail_nodes = check_dependencies(nodes, creds)
+    nodes_attributes_dict = init_nodes_attributes_dict(nodes)
+    node_dependencies_status = check_dependencies(nodes_attributes_dict, creds)
     banner_dependency_check()
-    report_skipped_nodes(dependency_fail_nodes)
-    cleanup_leftovers(nodes=dependency_pass_nodes, creds=creds)
-    banner_ipref()
-    results = all_to_all(link_layer=link_layer, node_list=dependency_pass_nodes, creds=creds)
+    qualified_nodes = node_qualification(node_dependencies_status, link_layer, metric_to_test)
+    cleanup_leftovers(nodes=nodes, creds=creds)
+    results = all_to_all(link_layer=link_layer, metric_to_test=metric_to_test, node_list=qualified_nodes, creds=creds)
     overall_avg_per_server = calc_avg(results)
     banner_results()
     sorted_results = sort_results(overall_avg_per_server)
@@ -252,9 +343,11 @@ def main():
     print("")
     print("")
 
-    cleanup_leftovers(nodes=dependency_pass_nodes, creds=creds)
+    cleanup_leftovers(nodes=qualified_nodes, creds=creds)
     print("")
     print("             <<<< Done >>>>                      ")
     print()
+
 if __name__ == '__main__':
     main()
+
