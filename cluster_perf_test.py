f36d57f6719465f82690fd8b64e085112fc4e2fe
ori-acoca <ori.acoca@weka.io>
2024-09-30 12:29:53 +0300

    Added dump to file in the local directory.

--- cluster_perf_test.py
+++ cluster_perf_test.py
@@ -2,12 +2,16 @@
 
 import os
 import sys
+
+
 from tabulate import tabulate
 import paramiko
 import time
 from colorama import Fore, Style
 import logging
 import argparse
+
+from datetime import datetime
 
 logging.basicConfig(
     level=logging.WARNING,  # log level
@@ -31,7 +35,7 @@ def banner(banner_name):
         "done": "Done"
     }
     print(f"\n{Fore.GREEN}============================================================================")
-    (print (f"{Fore.GREEN}{banner_map[banner_name]}"))
+    (print(f"{Fore.GREEN}{banner_map[banner_name]}"))
     print(
         f"{Fore.GREEN}============================================================================{Style.RESET_ALL}" + "\n")
 
@@ -40,17 +44,26 @@ def check_connection(nodes: list, user, key):
     responsive_nodes = []
     unresponsive_nodes = []
     for node in nodes:
-        uptime = execute_on_remote_nodes(node, "uptime", user, key, suppress_output=False)
-        if uptime:
-            print(f"Connection to {node} is successful")
-            responsive_nodes.append(node)
-        else:
-            print(f"Connection to {node} failed - Skipping")
+        try:
+            uptime = execute_on_remote_nodes(node, "uptime", user, key, suppress_output=False)
+            if uptime:
+                print(f"Connection to {node} is successful")
+                responsive_nodes.append(node)
+        except Exception as e:
+            logger.error(f"Connection to {node} failed with error: {e}")
             unresponsive_nodes.append(node)
+            continue
     return responsive_nodes, unresponsive_nodes
 
 
 def validate_private_key(key):
+    """
+    Function to validate that the private key is in the correct location and is indeed a private key
+
+    :param key: key's name
+    :return: pass so the script can continue
+    sys.exit() if the key is not found or not valid to stop the script
+    """
     try:
         with open(f"/root/.ssh/{key}") as f:
             if 'PRIVATE KEY' in f.read():
@@ -89,23 +102,47 @@ def get_args():
     return args
 
 
+def write_to_file(data_to_write: list, file_name: str):
+    """
+    Function to write data to a file
+
+    :param data_to_write: a list of data to write to a file
+    :param file_name: name of the file to write to
+    """
+
+
+    try:
+       with open(fr'./{file_name}', 'w') as f:
+            f.write('\n'.join(data_to_write))
+    except Exception as e:
+        logger.error(f"Error: can't write log to file {file_name} - {e}")
+
+
 def validate_nic_type(node, link_layer, user, key):
     """
     Function to validate that the NIC link_layer or of the server is indeed as declared by the user
+
     :param node:
     :param link_layer:
     :param user:
     :param key:
-    :return:
+    :return: Boolean value indicating if the NIC link_layer is as declared by the user
     """
-    actual_link_layer = execute_on_remote_nodes(host=node,
-                                                command=f"ip route show | grep {node} | awk '{{print $3}}' | xargs ip link show | grep link | awk '{{print $1}}'",
-                                                user=user, key=key, suppress_output=False)
-    if link_layer not in actual_link_layer:
-        logger.error(f"NIC with IP {node} is {actual_link_layer}, {link_layer} was expected")
-        return False
-    logger.info(f"Actual link layer on {node} is {actual_link_layer} - Passed link_layer validation")
-    return link_layer in actual_link_layer
+    command = f"ip route show | grep {node} | awk '{{print $3}}' | xargs ip link show | grep link | awk '{{print $1}}'"
+
+    try:
+        actual_link_layer = execute_on_remote_nodes(host=node, command=command, user=user, key=key,
+                                                    suppress_output=False)
+        if link_layer in actual_link_layer:
+            return True
+        elif link_layer not in actual_link_layer:
+            logger.error(f"NIC with IP {node} is {actual_link_layer}, {link_layer} was expected")
+            return False
+        else:
+            logger.info(f"link_layer on {node} can't be identified - Skipping")
+            return False
+    except Exception as e:
+        logger.error(f"Error: {e}")
 
 
 def execute_on_remote_nodes(host, command, user, key, suppress_output=True):
@@ -141,7 +178,6 @@ def execute_on_remote_nodes(host, command, user, key, suppress_output=True):
     return ""
 
 
-#
 def cleanup_leftovers(nodes: list, user, key, supress_notice=True):
     """
     Function to cleanup leftovers from previous tests
@@ -149,16 +185,17 @@ def cleanup_leftovers(nodes: list, user, key, supress_notice=True):
     :param nodes: list of nodes to executed a cleanup on
     :param user: username to use for SSH
     :param key: SSH key to use for authentication
-    :return: None
+    :param supress_notice: Boolean value to suppress cleanup notice
     """
     for node in nodes:
+        execute_on_remote_nodes(node, "lsof -n -i :18515 | awk '{print $2}' | grep -v PID | xargs kill -9", user,
+                                key,
+                                suppress_output=True)
+        execute_on_remote_nodes(node, "lsof -n -i :5001 | awk '{print $2}' | grep -v PID | xargs kill -9", user,
+                                key,
+                                suppress_output=True)
         if not supress_notice:
             print(f"Cleaning up leftovers on {node}")
-        else:
-            execute_on_remote_nodes(node, "lsof -n -i :18515 | awk '{print $2}' | grep -v PID | xargs kill -9", user, key,
-                                    suppress_output=True)
-            execute_on_remote_nodes(node, "lsof -n -i :5001 | awk '{print $2}' | grep -v PID | xargs kill -9", user, key,
-                                    suppress_output=True)
 
 
 def read_node_list(file):
@@ -211,8 +248,11 @@ def run_server(link_layer, metric_to_test, server, user, key):
     :param key: SSH key to use for authentication
     :return: None
     """
-    open_server = construct_server_command(link_layer, metric_to_test, server)
-    execute_on_remote_nodes(server, open_server, user, key, suppress_output=True)
+    try:
+        open_server = construct_server_command(link_layer, metric_to_test, server)
+        execute_on_remote_nodes(server, open_server, user, key, suppress_output=True)
+    except Exception as e:
+        logger.error(f"Error: {e}")
 
 
 def construct_client_command(link_layer, metric_to_test, server):
@@ -251,10 +291,13 @@ def run_client(link_layer, metric_to_test, client, server, user, key):
     :param key: SSH key to use for authentication
     :return: measured_perf - performance measured on the client during the test
     """
-    open_client = construct_client_command(link_layer, metric_to_test, server)
-    print(server + " <===> " + client)
-    measured_perf = round(float(execute_on_remote_nodes(client, open_client, user, key, suppress_output=False)), )
-    return measured_perf
+    try:
+        open_client = construct_client_command(link_layer, metric_to_test, server)
+        print(server + " <===> " + client)
+        measured_perf = round(float(execute_on_remote_nodes(client, open_client, user, key, suppress_output=False)), )
+        return measured_perf
+    except Exception as e:
+        logger.error(f"Error: {e}")
 
 
 def all_to_all(link_layer, metric_to_test, node_list: list, user, key):
@@ -343,8 +386,14 @@ def does_ib_send_bw_installed(node, user, key):
     :param key: SSH key to use for authentication
     :return: Boolean value indicating if ib_send_bw is installed on the node
     """
-    run_server("ib", "bw", node, user, key)
-    return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep ib_send_bw", user, key, suppress_output=False)
+    try:
+        run_server("ib", "bw", node, user, key)
+        return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep ib_send_bw", user, key,
+                                                   suppress_output=False)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        logger.error(f"can't evaluate dependencies for {node}")
+        return False
 
 
 def does_ib_send_lat_installed(node, user, key):
@@ -356,8 +405,14 @@ def does_ib_send_lat_installed(node, user, key):
     :param key: SSH key to use for authentication
     :return: Boolean value indicating if ib_send_lat is installed on the node
     """
-    run_server("ib", "lat", node, user, key)
-    return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep ib_send_lat", user, key, suppress_output=False)
+    try:
+        run_server("ib", "lat", node, user, key)
+        return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep ib_send_lat", user, key,
+                                                   suppress_output=False)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        logger.error(f"can't evaluate dependencies for {node}")
+        return False
 
 
 def does_iperf_installed(node, user, key):
@@ -369,8 +424,13 @@ def does_iperf_installed(node, user, key):
     :param key: SSH key to use for authentication
     :return: Boolean value indicating if iperf is installed on the node
     """
-    run_server("eth", "bw", node, user, key)
-    return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep iperf", user, key, suppress_output=False)
+    try:
+        run_server("eth", "bw", node, user, key)
+        return "LISTEN" in execute_on_remote_nodes(node, "ss -tulpn | grep iperf", user, key, suppress_output=False)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        logger.error(f"can't evaluate dependencies for {node}")
+        return False
 
 
 def node_qualification(node_list, link_layer, metric_to_test, user, key):
@@ -415,6 +475,8 @@ def main():
     nodes = read_node_list(args.nodes)
     validate_private_key(args.key)
     responsive_nodes, unresponsive_nodes = check_connection(nodes, args.user, args.key)
+    write_to_file(data_to_write=responsive_nodes, file_name="responsive_nodes.txt")
+    write_to_file(data_to_write=unresponsive_nodes, file_name="unresponsive_nodes.txt")
     banner("cleanup")
     cleanup_leftovers(nodes=responsive_nodes, user=args.user, key=args.key, supress_notice=False)
     banner("dependency_check")
